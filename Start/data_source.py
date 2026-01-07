941414ba7a718181e9da65353ff5a4771f71847d
Denny <58996965+DS-B-BB-G@users.noreply.github.com>
2026-01-07 07:53:04 +0000

    data sources

---
+++ Start/data_source.py
@@ -0,0 +1,4 @@
+print("Earthquake Hazards Program: https://www.usgs.gov/programs/earthquake-hazards")
+print("Earthquakes: https://www.usgs.gov/programs/earthquake-hazards/earthquakes")
+print("Real-time Data Services: https://earthquake.usgs.gov/earthquakes/feed/")
+print("GeoJSON Summary Format: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php")
