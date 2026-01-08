cfbee064d8f1a19f9c85e605f547744c818b9983
Denny <58996965+DS-B-BB-G@users.noreply.github.com>
2026-01-08 17:44:08 +0000

    add content

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,20 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
+from collections import defaultdict
 
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
-    data = json.load(datafile)
+def get_event_classification():
+    with open("../../30DayQuakes.json", "r") as datafile:
+        data = json.load(datafile)
+
+    # use a defaultdict to categorize
+    totals = defaultdict(int)
+    for event in data["features"]:
+        totals[event["properties"]["type"]] += 1
+
+    return dict(totals)
+
+result = get_event_classification()
+print(result)
